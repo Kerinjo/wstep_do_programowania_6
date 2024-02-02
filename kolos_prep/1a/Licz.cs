db93d81ce8db83cc77c7bd9b00877bd048ef2a5b
Konrad Marciniak <konrad.marciniak@microsoft.wsei.edu.pl>
2024-02-02 19:03:28 +0100

    zad1

---
+++ kolos_prep/1a/Licz.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace zad1
+{
+
+    internal class Licz
+    {
+        private int value;
+
+        public Licz(int value = 4)
+        {
+            this.value = value;
+        }
+        
+        public void Dodaj(int num = 4)
+        {
+            this.value += num;
+        }
+        
+        public void Odejmij(int num)
+        {
+            this.value -= num;
+        }
+
+        public void Wypisz()
+        {
+            Console.WriteLine($"wartosc: {this.value}");
+        }
+    }
+}
---
+++ kolos_prep/1a/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace zad1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Licz liczba = new Licz(7);
+
+            liczba.Wypisz();
+
+            liczba.Dodaj(6);
+            liczba.Wypisz();
+
+            liczba.Odejmij(14);
+            liczba.Wypisz();
+        }
+    }
+}
