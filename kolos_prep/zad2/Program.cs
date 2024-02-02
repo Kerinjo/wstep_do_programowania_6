6bd0c181a3207f7aff378860bd51c6165a50d42b
Konrad Marciniak <konrad.marciniak@microsoft.wsei.edu.pl>
2024-02-02 19:47:41 +0100

    zad2

---
+++ kolos_prep/zad2/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace zad2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int[] tab = new int[] { 1, 2, 3, 4, 5, 6 };
+            Sumator sumator = new Sumator(tab);
+
+            int res1 = sumator.Suma();
+            int res2 = sumator.SumaPodziel3();
+            int res3 = sumator.IleElementow();
+
+            Console.WriteLine($"Suma1: {res1}\tSuma2: {res2}");
+            Console.WriteLine($"Dlugosc: {res3}");
+
+            sumator.WypiszElementy();
+            sumator.FinalBoss(1, 5);
+        }
+    }
+}
---
+++ kolos_prep/zad2/Sumator.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace zad2
+{
+    internal class Sumator
+    {
+        private int[] Liczby;
+
+        public Sumator(int[] tab)
+        {
+            Liczby = tab;
+        }
+
+        public int Suma()
+        {
+            int sum = 0;
+            foreach (var num in Liczby)
+            {
+                sum += num;
+            }
+
+            return sum;
+        }
+
+        public int SumaPodziel3()
+        {
+            int sum = 0;
+            foreach (var num in Liczby)
+            {
+                if (num % 3 == 0)
+                    sum += num;
+            }
+
+            return sum;
+        }
+        
+        public int IleElementow() => Liczby.Length; 
+
+        public void WypiszElementy()
+        {
+            string out_str = "[";
+
+            foreach (var val in Liczby)
+            {
+                out_str += val.ToString();
+                out_str += ", ";
+            }
+
+            out_str = out_str.Remove(out_str.Length - 2);
+
+            out_str += "]";
+            Console.WriteLine(out_str);
+        }
+
+        public void FinalBoss(int lowIndex, int highIndex)
+        {
+            if (lowIndex < 0)
+                lowIndex = 0;
+            if (highIndex > Liczby.Length - 1)
+                highIndex = Liczby.Length - 1;
+
+            string out_str = "[";
+            for (int i = lowIndex; i <= highIndex; i++)
+            {
+                Console.WriteLine($"{i}, {Liczby[i]}");
+                out_str += Liczby[i].ToString();
+                out_str += ", ";
+            
+            }
+
+            out_str = out_str.Remove(out_str.Length - 2);
+
+            out_str += "]";
+            Console.WriteLine(out_str);
+        }
+    }
+}
